5cd15c2e1141bc80f0920b67179d4bc2d4c7c86f
Chisaka <s1000628@sendai-nct.jp>
2014-10-28 18:13:26 +0900

    画面を描画するための Snake(JFrame) と SnakePanel(JPanel) を作成

--- src/Snake.java
+++ src/Snake.java
@@ -1,7 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class Snake {
+public class Snake extends JFrame {
 
 	public static void main(String[] args) {
+		Snake snake = new Snake();
+		snake.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		snake.setSize(640, 480);
+		snake.setResizable(false);
+		snake.setLocationByPlatform(true);
+		snake.add(new SnakePanel(), BorderLayout.CENTER);
+		snake.setVisible(true);
 	}
 
 }
---
+++ src/SnakePanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+
+public class SnakePanel extends JPanel {
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+	}
+
+}
