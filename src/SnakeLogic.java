e98bc458198ada9b23e7f0ace7e2b4001ed25ae7
Chisaka <s1000628@sendai-nct.jp>
2014-10-30 16:09:05 +0900

    SnakeLogic の reset メソッドと step メソッドを実装

--- src/SnakeLogic.java
+++ src/SnakeLogic.java
@@ -3,11 +3,56 @@ import java.util.*;
 public class SnakeLogic {
 	
 	public void step(Direction input) {
-		throw new UnsupportedOperationException();
+		// 既にゲームオーバーなら何もしない
+		if (gameover) {
+			return;
+		}
+		
+		// 現在の頭の位置を tails に追加する
+		tails.addFirst(head);
+		
+		// 長さを揃える
+		while (tails.size() > len - 1) {
+			tails.removeLast();
+		}
+		
+		// 入力に合わせて向きを変える
+		d = input;
+		
+		// 向いている方向に1つ進む
+		if (d == Direction.RIGHT) {
+			++head.x;
+		} else if (d == Direction.UP) {
+			--head.y;
+		} else if (d == Direction.LEFT) {
+			--head.x;
+		} else {
+			++head.y;
+		}
+		
+		// 画面外に出たらゲームオーバー
+		if (head.x < 0 || head.y < 0 || head.x >= w || head.y >= h) {
+			gameover = true;
+		}
+		
+		// 自分の身体にぶつかったらゲームオーバー
+		for (Point2D tail : tails) {
+			if (head.equals(tail)) {
+				gameover = true;
+				break;
+			}
+		}
 	}
 	
 	public void reset() {
-		throw new UnsupportedOperationException();
+		Random r = new Random();
+		
+		gameover = false;
+		
+		tails.clear();
+		
+		head.x = r.nextInt(w);
+		head.y = r.nextInt(h);
 	}
 	
 	public int getWidth() {
@@ -52,9 +97,19 @@ public class SnakeLogic {
 		return (LinkedList<Point2D>)tails.clone();
 	}
 	
+	public int getLength() {
+		return len;
+	}
+	
+	public void setLength(int length) {
+		len = length;
+	}
+	
 	private int w = 16, h = 16;
 	private Point2D head = new Point2D(1, 1);
 	private Direction d = Direction.RIGHT;
 	private LinkedList<Point2D> tails = new LinkedList<Point2D>();
+	private int len = 3;
+	private boolean gameover = false;
 
 }
